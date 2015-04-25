16cff725e0fa03572425de9f5e163221dcead808
tkell <thor.kell@mail.mcgill.ca>
2015-04-25 15:28:25 -0400

    trying to get redis poping and pushing

--- trace_server.py
+++ trace_server.py
@@ -10,13 +10,18 @@ from flask.ext.cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
+
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis = redis.from_url(redis_url)
+trace_list = 'traces'
 
 @app.route("/route")
 def get_route():
     # get the sound from our database
-    route = [{'a': 'yes', 'b': 'no'}, {'c': 'maybe'}]
+    redis.rpush(trace_list, "this one!")
+    redis.rpush(trace_list, "not this one")
+    val = redis.lpop(trace_list)
+    route = [{'a': 'yes', 'b': 'no'}, {'c': val}]
     res = json.dumps(route)
     return res
 
