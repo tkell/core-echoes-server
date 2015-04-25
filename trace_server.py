4a600ddd3dcc60f468792875a5266e3f547e18fa
tkell <thor.kell@mail.mcgill.ca>
2015-04-25 16:25:23 -0400

    other routes

--- trace_server.py
+++ trace_server.py
@@ -15,14 +15,30 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis = redis.from_url(redis_url)
 trace_list = 'traces'
 
-@app.route("/route")
-# Gets the current trace
+@app.route("/route", methods=['GET'])
 def get_route():
+    '''Gets the current trace'''
     val = redis.lrange(trace_list, 0, 1)
     route = {'data': val}
     res = json.dumps(route)
     return res
 
+@app.route("/route", methods=['POST'])
+def add_route():
+    '''Add a new trace to the back of the queue'''
+    val = redis.rpush(trace_list, request.data)
+    route = {'data': val}
+    res = json.dumps(route)
+    return res
+
+@app.route("/route", methods=['DELETE'])
+def delete_route():
+    '''Deletes the trace from the front of the queue'''
+    val = redis.lpop(trace_list)
+    route = {'data': val}
+    res = json.dumps(route)
+    return res
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
