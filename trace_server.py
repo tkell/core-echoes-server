38a111205e6755c242c298ed3c93285d8f8e6c13
tkell <thor.kell@mail.mcgill.ca>
2015-05-30 11:28:45 -0400

    Debugging, yay heroku

--- trace_server.py
+++ trace_server.py
@@ -24,7 +24,9 @@ last_ip = 'last_ip'
 def status():
     '''returns the status'''
     count = redis.llen(trace_list)
+    print "trying to get last ip"
     last_ip = redis.get(last_ip)
+    print count, last_ip
     return "Online:  %d traces in the db.  Last sent IP is %s" % (count, last_ip)
 
 @app.route("/count", methods=['GET'])
